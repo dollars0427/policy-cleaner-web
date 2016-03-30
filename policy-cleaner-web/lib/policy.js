e6d452a3292bdb90ad8444151059f326b702159e
Sardo <sardo.ip@nxtomo.hk>
2016-03-30 13:02:36 +0800

    Add policy.js and route of delete policy

---
+++ policy-cleaner-web/lib/policy.js
@@ -0,0 +1,65 @@
+/*
+ * Get Polices from AWS
+ *
+ * @param {object} iam
+ * @param {object} params
+ * @param {function} cb
+ *
+ */
+
+function getPolicies(iam, params, cb){
+
+    iam.listPolicies(params, function(err, data){
+
+        if(err){
+            cb(err, null);
+            return;
+        }
+
+        var policies = [];
+
+        for (var i = 0; i < data.Policies.length; i++){
+
+            var policy = {
+                'policyName':data.Policies[i].PolicyName, 
+                'policyArn': data.Policies[i].Arn, 
+                'createDate':data.Policies[i].CreateDate
+            };
+
+            policies.push(policy);
+        }
+
+        cb(null, policies);
+
+    });
+}
+
+/*
+ * Delete Polices from AWS
+ *
+ * @param {object} iam
+ * @param {object} params
+ * @param {function} cb
+ *
+ */
+
+function deletePolicy(iam, params, cb){
+
+    iam.deletePolicy(params, function(err, data){
+
+        if(err){
+            cb(err, null);
+            return;
+        }
+
+        cb(null);
+
+    });
+}
+
+
+
+module.exports = {
+    getPolicies:getPolicies
+    deletePolicy:deletePolicy
+}
--- policy-cleaner-web/routes.js
+++ policy-cleaner-web/routes.js
@@ -1,16 +1,63 @@
 var express = require('express');
-
 var log4js = require('log4js');
-var logger = log4js.getLogger('Routes');
+var aws = require('aws-sdk');
+var nconf = require('nconf');
 
+var policy = require(__dirname + '/lib/policy');
+
+var logger = log4js.getLogger('Routes');
 var router = express.Router();
+
+var accessKey = nconf.get('token').access_key;
+var accessSecert = nconf.get('token').access_secret;
+
+aws.config.update({accessKeyId:accessKey, secretAccessKey:accessSecert});
 
 function policyList(req, res){
 
-    res.render('policylist');
+    var iam = new aws.IAM();
 
+    var params = {
+        Scope:'All'
+    }
+
+    policy.getPolicies(iam, params, function(err, policies){
+
+        if(err){
+            logger.error(err, err.stack);
+            res.render('policylist',{policies:null});
+            return;
+        }
+
+        res.render('policylist', {policies:policies});
+    });
+}
+
+function deletePolicies(req, res){
+
+    var policies = req.body.policies;
+
+    for(var i = 0; i < policies.length; i++;){
+
+        var policyArn = policies[i].policyArn;
+
+        var params = {PolicyArn:policyArn};
+
+        policy.deletePolicies(params, function(err){
+
+            if(err){
+                logger.error(err, err.stack);
+                return;
+            }
+
+            logger.info(policies[i].PolicyName + ' Deleted!');
+
+        });
+    }
 }
 
 router.get('/', policyList);
+
+router.post('/delete', deletePolicies);
 
 module.exports = router;
